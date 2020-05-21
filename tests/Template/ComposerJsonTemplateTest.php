c3926e85b2e2b801d2c1a5831a8b014f7c0ed38f
Shudd3r <q3.shudder@gmail.com>
2020-05-21 16:12:16 +0200

    Added tests for ComposerJsonTemplate

---
+++ tests/Template/ComposerJsonTemplateTest.php
@@ -0,0 +1,104 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of Shudd3r/Package-Files package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Shudd3r\PackageFiles\Tests\Template;
+
+use PHPUnit\Framework\TestCase;
+use Shudd3r\PackageFiles\Template\ComposerJsonTemplate;
+use Shudd3r\PackageFiles\Tests\Doubles;
+
+
+class ComposerJsonTemplateTest extends TestCase
+{
+    public function testEmptyComposerFileIsFilledWithPropertiesValues()
+    {
+        $renderedString = $this->template('{}')->render(new Doubles\FakeProperties());
+
+        $expectedString = <<<'JSON'
+            {
+                "name": "polymorphine/dev",
+                "description": "Package description",
+                "type": "library",
+                "license": "MIT",
+                "authors": [
+                    {
+                        "name": "Shudd3r",
+                        "email": "q3.shudder@gmail.com"
+                    }
+                ],
+                "autoload": {
+                    "psr-4": {
+                        "Polymorphine\\Dev\\": "src/"
+                    }
+                },
+                "autoload-dev": {
+                    "psr-4": {
+                        "Polymorphine\\Dev\\Tests\\": "tests/"
+                    }
+                },
+                "minimum-stability": "stable"
+            }
+            
+            JSON;
+
+        $this->assertEquals($expectedString, $renderedString);
+    }
+
+    public function testFieldsAreReturnedInCorrectOrder()
+    {
+        $template = [
+            'require'           => [],
+            'require-dev'       => [],
+            'type'              => 'xxx',
+            'license'           => 'xxx',
+            'name'              => 'xxx',
+            'description'       => 'xxx',
+            'autoload'          => [],
+            'autoload-dev'      => [],
+            'authors'           => [['name' => 'Shudd3r', 'email' => 'q3.shudder@gmail.com']],
+            'minimum-stability' => 'stable'
+        ];
+
+        $renderedString    = $this->template(json_encode($template))->render(new Doubles\FakeProperties());
+        $composerArrayKeys = array_keys(json_decode($renderedString, true));
+
+        $expectedKeys = [
+            'name', 'description', 'type', 'license', 'authors', 'autoload', 'autoload-dev',
+            'minimum-stability', 'require', 'require-dev'
+        ];
+
+        $this->assertEquals($expectedKeys, $composerArrayKeys);
+    }
+
+    public function testSrcAutoloadIsUpdated()
+    {
+        $autoload = json_encode([
+            'autoload'     => ['foo' => ['bar'], 'psr-4' => ['Foo\\Namespace\\' => 'src/']],
+            'autoload-dev' => ['psr-4' => ['Foo\\Namespace\\Tests' => 'tests/']]
+        ]);
+
+        $renderedString = $this->template($autoload)->render(new Doubles\FakeProperties());
+        $composerArray  = json_decode($renderedString, true);
+
+        $expected = [
+            'autoload'     => ['psr-4' => ['Polymorphine\\Dev\\' => 'src/'], 'foo' => ['bar']],
+            'autoload-dev' => ['psr-4' => ['Polymorphine\\Dev\\Tests\\' => 'tests/']]
+        ];
+
+        $this->assertSame($expected['autoload'], $composerArray['autoload']);
+        $this->assertSame($expected['autoload-dev'], $composerArray['autoload-dev']);
+    }
+
+    private function template(string $contents)
+    {
+        return new ComposerJsonTemplate(new Doubles\MockedFile($contents));
+    }
+}
