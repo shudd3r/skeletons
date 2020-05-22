115777fd2de62469d7539e9349f263c3ce082a05
Shudd3r <q3.shudder@gmail.com>
2020-05-23 01:29:19 +0200

    Added method call counters to FakeProperties

--- tests/Doubles/FakeProperties.php
+++ tests/Doubles/FakeProperties.php
@@ -16,30 +16,59 @@ use Shudd3r\PackageFiles\Properties;
 
 class FakeProperties extends Properties
 {
-    private array $properties;
+    public array $propertiesCalled = [
+        'repositoryUrl'      => 0,
+        'repositoryName'     => 0,
+        'packageName'        => 0,
+        'packageDescription' => 0,
+        'sourceNamespace'    => 0
+    ];
+
+    public array $properties = [
+        'repositoryUrl'      => 'https://github.com/polymorphine/dev.git',
+        'repositoryName'     => null,
+        'packageName'        => 'polymorphine/dev',
+        'packageDescription' => 'Package description',
+        'sourceNamespace'    => 'Polymorphine\Dev'
+    ];
 
     public function __construct(array $properties = [])
     {
-        $this->properties = $properties;
+        $this->properties = $properties + $this->properties;
     }
 
     public function repositoryUrl(): string
     {
-        return $this->properties['repositoryUrl'] ??= 'https://github.com/polymorphine/dev.git';
+        return $this->get('repositoryUrl');
+    }
+
+    public function repositoryName(): string
+    {
+        if (!isset($this->properties['repositoryName'])) {
+            $this->properties['repositoryName'] = parent::repositoryName();
+        }
+
+        return $this->get('repositoryName');
     }
 
     public function packageName(): string
     {
-        return $this->properties['packageName'] ??= 'polymorphine/dev';
+        return $this->get('packageName');
     }
 
     public function packageDescription(): string
     {
-        return $this->properties['packageDescription'] ??= 'Package description';
+        return $this->get('packageDescription');
     }
 
     public function sourceNamespace(): string
     {
-        return $this->properties['sourceNamespace'] ??= 'Polymorphine\Dev';
+        return $this->get('sourceNamespace');
+    }
+
+    private function get(string $key): string
+    {
+        $this->propertiesCalled[$key]++;
+        return $this->properties[$key];
     }
 }
