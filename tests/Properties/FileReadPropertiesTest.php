d7500c628c5789be2bd9bc33caf3a4b9e43b7ebb
Shudd3r <q3.shudder@gmail.com>
2020-05-22 12:22:08 +0200

    Added tests for FileReadProperties

---
+++ tests/Properties/FileReadPropertiesTest.php
@@ -0,0 +1,94 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of Shudd3r/Package-Files package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Shudd3r\PackageFiles\Tests\Properties;
+
+use PHPUnit\Framework\TestCase;
+use Shudd3r\PackageFiles\Properties\FileReadProperties;
+use Shudd3r\PackageFiles\Tests\Doubles\FakeDirectory;
+
+
+class FileReadPropertiesTest extends TestCase
+{
+    private ?FakeDirectory $directory;
+
+    public function testGithubUrlIsReadFromGitConfigFile()
+    {
+        $properties = $this->properties();
+        $this->assertSame('', $properties->repositoryUrl());
+
+        $this->directory->files['.git/config'] = <<<'INI'
+            [core]
+                repositoryformatversion = 0
+                filemode = false
+                bare = false
+                logallrefupdates = true
+                symlinks = false
+                ignorecase = true
+            [remote "origin"]
+                url = https://github.com/username/repository.git
+                fetch = +refs/heads/*:refs/remotes/origin/*
+            [branch "develop"]
+                remote = origin
+                merge = refs/heads/develop
+            
+            INI;
+
+        $this->assertSame('https://github.com/username/repository.git', $properties->repositoryUrl());
+    }
+
+    public function testGithubUrlForUpstreamRepositoryTakesPrecedence()
+    {
+        $properties = $this->properties();
+        $this->directory->files['.git/config'] = <<<'INI'
+            [remote "origin"]
+                url = https://github.com/username/repositoryOrigin.git
+                fetch = +refs/heads/*:refs/remotes/origin/*
+            [remote "upstream"]
+                url = https://github.com/username/repositoryUpstream.git
+                fetch = +refs/heads/*:refs/remotes/origin/*
+            INI;
+
+        $this->assertSame('https://github.com/username/repositoryUpstream.git', $properties->repositoryUrl());
+    }
+
+    public function testValuesReadFromComposerJsonFile()
+    {
+        $properties = $this->properties();
+        $this->assertSame('', $properties->packageName());
+        $this->assertSame('', $properties->packageDescription());
+        $this->assertSame('', $properties->sourceNamespace());
+
+        $properties = $this->properties();
+        $this->directory->files['composer.json'] = <<<'JSON'
+            {
+                "name": "package/name",
+                "description": "Test description",
+                "autoload": {
+                    "psr-4": {
+                        "Foo\\Bar\\": "src/",
+                        "Something\\Else\\": "test/"
+                    }
+                }
+            }
+            
+            JSON;
+
+        $this->assertSame('package/name', $properties->packageName());
+        $this->assertSame('Test description', $properties->packageDescription());
+        $this->assertSame('Foo\\Bar', $properties->sourceNamespace());
+    }
+
+    private function properties()
+    {
+        return new FileReadProperties($this->directory ??= new FakeDirectory());
+    }
+}
