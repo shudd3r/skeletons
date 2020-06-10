d652fbad1fadcc63e57e1c5254e6f0a65d32813a
Shudd3r <q3.shudder@gmail.com>
2020-06-10 13:39:31 +0200

    Added functional tests for resolving properties

--- tests/Command/InitCommandTest.php
+++ tests/Command/InitCommandTest.php
@@ -27,6 +27,121 @@ class InitCommandTest extends TestCase
         $this->assertInstanceOf(RuntimeEnv::class, $this->env);
     }
 
+    public function testPropertiesAreReadFromProjectFiles()
+    {
+        $factory = $this->factory();
+        $composer = json_encode([
+            'name'        => 'fooBar/baz',
+            'description' => 'My library package',
+            'autoload'    => ['psr-4' => ['FooBarNamespace\\Baz\\' => 'src/']]
+        ]);
+        $this->env->packageFiles()->files['composer.json'] = new Doubles\MockedFile($composer);
+        $iniData = '[remote "origin"] url = https://github.com/username/repositoryOrigin.git';
+        $this->env->packageFiles()->files['.git/config'] = new Doubles\MockedFile($iniData);
+
+        $factory->command(['i' => false])->execute();
+
+        $this->assertMetaDataFile([
+            'original_repository' => 'https://github.com/username/repositoryOrigin.git',
+            'package_name'        => 'fooBar/baz',
+            'package_desc'        => 'My library package',
+            'source_namespace'    => 'FooBarNamespace\\Baz'
+        ]);
+    }
+
+    public function testUnresolvedPropertiesAreReadFromDirectoryNames()
+    {
+        $command = $this->factory()->command(['i' => false]);
+        $this->env->packageFiles()->path = '/path/foo/bar';
+
+        $command->execute();
+
+        $this->assertMetaDataFile([
+            'original_repository' => 'https://github.com/foo/bar.git',
+            'package_name'        => 'foo/bar',
+            'package_desc'        => 'foo/bar package',
+            'source_namespace'    => 'Foo\\Bar'
+        ]);
+    }
+
+    public function testEstablishedPackageNameWillDetermineOtherProperties()
+    {
+        $factory  = $this->factory();
+        $composer = json_encode(['name' => 'fooBar/baz']);
+        $this->env->packageFiles()->files['composer.json'] = new Doubles\MockedFile($composer);
+
+        $factory->command(['i' => false])->execute();
+
+        $this->assertMetaDataFile([
+            'original_repository' => 'https://github.com/fooBar/baz.git',
+            'package_name'        => 'fooBar/baz',
+            'package_desc'        => 'fooBar/baz package',
+            'source_namespace'    => 'FooBar\\Baz'
+        ]);
+    }
+
+    public function testCommandLineDefinedPropertiesHavePriorityOverResolved()
+    {
+        $factory = $this->factory();
+
+        $factory->command([])->execute();
+        $this->assertMetaDataFile([
+            'original_repository' => 'https://github.com/package/directory.git',
+            'package_name'        => 'package/directory',
+            'package_desc'        => 'package/directory package',
+            'source_namespace'    => 'Package\Directory'
+        ]);
+
+        $options = [
+            'i'       => true,
+            'repo'    => 'https://github.com/cli/repo.git',
+            'package' => 'cli/package',
+            'desc'    => 'cli desc',
+            'ns'      => 'Cli\NamespaceX'
+        ];
+
+        $factory->command($options)->execute();
+        $this->assertMetaDataFile([
+            'original_repository' => 'https://github.com/cli/repo.git',
+            'package_name'        => 'cli/package',
+            'package_desc'        => 'cli desc',
+            'source_namespace'    => 'Cli\NamespaceX'
+        ]);
+    }
+
+    public function testInteractiveInputStringsWillOverwriteAllProperties()
+    {
+        $factory = $this->factory();
+        $this->env->input()->inputStrings = [
+            'https://github.com/user/repo.git',
+            'package/name',
+            'My\Namespace',
+            'package input description'
+        ];
+
+        $options = [
+            'i'       => true,
+            'repo'    => 'https://github.com/cli/repo.git',
+            'package' => 'cli/package',
+            'desc'    => 'cli desc',
+            'ns'      => 'Cli\NamespaceX'
+        ];
+
+        $factory->command($options)->execute();
+        $this->assertMetaDataFile([
+            'original_repository' => 'https://github.com/user/repo.git',
+            'package_name'        => 'package/name',
+            'package_desc'        => 'package input description',
+            'source_namespace'    => 'My\Namespace'
+        ]);
+    }
+
+    private function assertMetaDataFile(array $data): void
+    {
+        $metaDataFile = $this->env->packageFiles()->files['.github/package.properties']->contents();
+        $this->assertSame($data, parse_ini_string($metaDataFile));
+    }
+
     private function factory(): Command\Factory
     {
         $terminal = new Doubles\MockedTerminal();
