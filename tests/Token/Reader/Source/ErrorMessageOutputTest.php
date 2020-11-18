6b9029d03b6f42c440eaec167c2b02eafdc1b6be
Shudd3r <q3.shudder@gmail.com>
2020-11-18 17:12:25 +0100

    Added Source wrapper for error message output

---
+++ tests/Token/Reader/Source/ErrorMessageOutputTest.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of Shudd3r/Package-Files package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Shudd3r\PackageFiles\Tests\Token\Reader\Source;
+
+use PHPUnit\Framework\TestCase;
+use Shudd3r\PackageFiles\Token\Reader\Source\ErrorMessageOutput;
+use Shudd3r\PackageFiles\Tests\Doubles;
+
+
+class ErrorMessageOutputTest extends TestCase
+{
+    public function testMissingTokenFromSource_SendsErrorMessageToOutput()
+    {
+        $output = new Doubles\MockedTerminal();
+        $source = new ErrorMessageOutput(new Doubles\FakeSource(null), $output, 'MyToken');
+
+        $this->assertNull($source->create('foo'));
+        $this->assertSame(['Invalid MyToken value: `foo`'], $output->messagesSent);
+    }
+
+    public function testValue_ReturnsValueFromWrappedSource()
+    {
+        $source = new ErrorMessageOutput(new Doubles\FakeSource('foo'), new Doubles\MockedTerminal(), 'MyToken');
+        $this->assertSame('foo', $source->value());
+    }
+}
