c16f0bfde3fbca1c971541d36789897efb1c1220
Shudd3r <q3.shudder@gmail.com>
2020-11-19 02:25:15 +0100

    Added CachedValue Source wrapper

---
+++ src/Token/Reader/Source/CachedValue.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of Shudd3r/Package-Files package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Shudd3r\PackageFiles\Token\Reader\Source;
+
+use Shudd3r\PackageFiles\Token\Reader\Source;
+use Shudd3r\PackageFiles\Token;
+
+
+class CachedValue implements Source
+{
+    private Source $source;
+    private ?string $value = null;
+
+    public function __construct(Source $source)
+    {
+        $this->source = $source;
+    }
+
+    public function create(string $value): ?Token
+    {
+        return $this->source->create($value);
+    }
+
+    public function value(): string
+    {
+        return $this->value ??= $this->source->value();
+    }
+}
