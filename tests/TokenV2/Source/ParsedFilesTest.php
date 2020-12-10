2286de41d36713bd3f4d1e535c61f7160aeb9902
Shudd3r <q3.shudder@gmail.com>
2020-12-10 14:07:58 +0100

    Added ParsedFiles Source implementation

---
+++ tests/TokenV2/Source/ParsedFilesTest.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of Shudd3r/Package-Files package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Shudd3r\PackageFiles\Tests\TokenV2\Source;
+
+use PHPUnit\Framework\TestCase;
+use Shudd3r\PackageFiles\TokenV2\Source\ParsedFiles;
+use Shudd3r\PackageFiles\Tests\Doubles;
+
+
+class ParsedFilesTest extends TestCase
+{
+    public function testSource_ValueMethod_ReturnsParserValue()
+    {
+        $source = new ParsedFiles();
+        $parser = new Doubles\MockedValueToken();
+        $this->assertSame('parsed value', $source->value($parser));
+    }
+}
