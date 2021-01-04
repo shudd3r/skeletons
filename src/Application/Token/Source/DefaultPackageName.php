35e846a4131fce20d9b78d7f008bca584ffde833
Shudd3r <q3.shudder@gmail.com>
2021-01-04 05:13:05 +0100

    Extracted default value from Reader classes to Source

---
+++ src/Application/Token/Source/DefaultPackageName.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of Shudd3r/Package-Files package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Shudd3r\PackageFiles\Application\Token\Source;
+
+use Shudd3r\PackageFiles\Application\Token\Source;
+use Shudd3r\PackageFiles\Application\Token\Reader\Data\ComposerJsonData;
+use Shudd3r\PackageFiles\Environment\FileSystem\Directory;
+use Shudd3r\PackageFiles\Application\Token\Parser;
+
+
+class DefaultPackageName implements Source
+{
+    private ComposerJsonData $composer;
+    private Directory        $project;
+
+    public function __construct(ComposerJsonData $composer, Directory $project)
+    {
+        $this->composer = $composer;
+        $this->project  = $project;
+    }
+
+    public function value(Parser $parser): string
+    {
+        return $this->composer->value('name') ?? $this->directoryFallback();
+    }
+
+    private function directoryFallback(): string
+    {
+        $path = $this->project->path();
+        return $path ? basename(dirname($path)) . '/' . basename($path) : '';
+    }
+}
