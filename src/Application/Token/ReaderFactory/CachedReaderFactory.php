29c82def6e4112eab872e3dfdb5ca60adb411708
Shudd3r <q3.shudder@gmail.com>
2021-01-09 18:25:37 +0100

    Added CachedReaderFactory class

---
+++ src/Application/Token/ReaderFactory/CachedReaderFactory.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of Shudd3r/Package-Files package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Shudd3r\PackageFiles\Application\Token\ReaderFactory;
+
+use Shudd3r\PackageFiles\Application\Token\ReaderFactory;
+use Shudd3r\PackageFiles\Application\Token\Reader;
+
+
+class CachedReaderFactory implements ReaderFactory
+{
+    private ReaderFactory $factory;
+
+    private Reader $initializeReader;
+    private Reader $validationReader;
+    private Reader $updateReader;
+
+    public function __construct(ReaderFactory $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    public function initializationReader(): Reader
+    {
+        return $this->initializeReader ??= $this->factory->initializationReader();
+    }
+
+    public function validationReader(): Reader
+    {
+        return $this->validationReader ??= $this->factory->validationReader();
+    }
+
+    public function updateReader(): Reader
+    {
+        return $this->updateReader ??= $this->factory->updateReader();
+    }
+}
