4f54bf956b3f4ce5194ebb098bcef01dc7506886
Shudd3r <q3.shudder@gmail.com>
2021-02-14 00:11:43 +0100

    Added Replacements container

---
+++ src/Application/Token/Replacements.php
@@ -0,0 +1,56 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of Shudd3r/Package-Files package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Shudd3r\PackageFiles\Application\Token;
+
+use Shudd3r\PackageFiles\Application\RuntimeEnv;
+use RuntimeException;
+
+
+class Replacements
+{
+    private RuntimeEnv $env;
+    private array      $replacements = [];
+
+    public function __construct(RuntimeEnv $env)
+    {
+        $this->env = $env;
+    }
+
+    public function addReplacement(string $name, callable $createMethod): void
+    {
+        $this->replacements[$name] = $createMethod($this->env);
+    }
+
+    public function replacement(string $name)
+    {
+        if (!$this->replacements[$name]) {
+            throw new RuntimeException(sprintf('Replacement `%s` is not defined', $name));
+        }
+
+        return $this->replacements[$name];
+    }
+
+    public function init(array $options): Reader
+    {
+        return new Reader\InitialReader($this->replacements, $options);
+    }
+
+    public function validate(): Reader
+    {
+        return new Reader\ValidationReader($this->replacements);
+    }
+
+    public function update(array $options): Reader
+    {
+        return new Reader\UpdateReader($this->replacements, $options);
+    }
+}
