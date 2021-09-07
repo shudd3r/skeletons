a70b2f5d8ee00d2d0b2d6d79b44d5cb39ddcb952
Shudd3r <q3.shudder@gmail.com>
2021-09-07 09:45:38 +0200

    Added directory comparison tools to ExampleFiles

--- tests/Fixtures/ExampleFiles.php
+++ tests/Fixtures/ExampleFiles.php
@@ -11,18 +11,54 @@
 
 namespace Shudd3r\PackageFiles\Tests\Fixtures;
 
+use Shudd3r\PackageFiles\Environment\FileSystem\Directory;
+use Shudd3r\PackageFiles\Tests\Doubles\FakeDirectory;
+
 
 class ExampleFiles
 {
-    private string $directory;
+    private Directory $directory;
 
     public function __construct(string $directory)
     {
-        $this->directory = __DIR__ . DIRECTORY_SEPARATOR . $directory;
+        $this->directory = new Directory\LocalDirectory(__DIR__ . DIRECTORY_SEPARATOR . $directory);
+    }
+
+    public function directory(string $name): FakeDirectory
+    {
+        $fakeDirectory = new FakeDirectory('/' . $name);
+        foreach ($this->directory->subdirectory($name)->files() as $file) {
+            $fakeDirectory->addFile($file->name(), $file->contents());
+        }
+
+        return $fakeDirectory;
+    }
+
+    public function hasSameFilesAs(Directory $directory): bool
+    {
+        $givenFiles = $directory->files();
+
+        if (count($givenFiles) !== count($this->directory->files())) {
+            echo 'Directories are not the same: File number mismatch';
+            return false;
+        }
+
+        foreach ($givenFiles as $file) {
+            $givenContents     = $file->contents();
+            $directoryContents = $this->directory->file($file->name())->contents();
+
+            if ($givenContents !== $directoryContents) {
+                echo 'expected: ---------------' . PHP_EOL . $directoryContents . PHP_EOL;
+                echo 'given: ------------------' . PHP_EOL . $givenContents . PHP_EOL;
+                echo '-------------------------';
+                return false;
+            }
+        }
+        return true;
     }
 
     public function contentsOf(string $filename): string
     {
-        return file_get_contents($this->directory . DIRECTORY_SEPARATOR . $filename);
+        return $this->directory->file($filename)->contents();
     }
 }
