49d9170d117edd817fbbfe62eaadd4af8a2ac2a5
Shudd3r <q3.shudder@gmail.com>
2021-09-21 14:37:29 +0200

    Changed callback arguments for custom templates

--- tests/ApplicationTest.php
+++ tests/ApplicationTest.php
@@ -173,7 +173,10 @@ class ApplicationTest extends TestCase
         $replacements->add(self::PACKAGE_DESC, new Replacement\PackageDescription($env, $packageName));
         $replacements->add(self::SRC_NAMESPACE, new Replacement\SrcNamespace($env, $packageName));
 
-        $jsonMerge = fn (Template $template, File $composer) => new Template\MergedJsonTemplate($template, $composer);
+        $jsonMerge = fn (File $template) => new Template\MergedJsonTemplate(
+            new Template\FileTemplate($template),
+            $env->package()->file($template->name())
+        );
         $env->addTemplate('composer.json', $jsonMerge);
 
         return $env;
