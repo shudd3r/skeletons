a270265b48c419f7f18b823b203a548fe2194f32
Shudd3r <q3.shudder@gmail.com>
2021-10-14 00:08:14 +0200

    Added redesigned Reader & Replacements classes

---
+++ src/Application/Token/Reworked/Reader.php
@@ -0,0 +1,82 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of Shudd3r/Package-Files package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Shudd3r\PackageFiles\Application\Token\Reworked;
+
+use Shudd3r\PackageFiles\Application\RuntimeEnv;
+use Shudd3r\PackageFiles\Application\Token;
+use Shudd3r\PackageFiles\Application\Token\ValueToken;
+use Shudd3r\PackageFiles\Replacement;
+
+
+abstract class Reader
+{
+    protected Replacements $replacements;
+    protected RuntimeEnv   $env;
+
+    /** @var ?ValueToken[] */
+    protected array $tokens = [];
+
+    private array $options;
+    private bool  $interactive;
+
+    public function __construct(Replacements $replacements, RuntimeEnv $env, array $options)
+    {
+        $this->replacements = $replacements;
+        $this->env          = $env;
+        $this->options      = $options;
+        $this->interactive  = isset($options['i']) || isset($options['interactive']);
+    }
+
+    public function token(): ?Token
+    {
+        $tokens = $this->tokens();
+        return !in_array(null, $tokens) ? new Token\CompositeToken(...array_values($tokens)) : null;
+    }
+
+    public function tokenValues(): array
+    {
+        $values = [];
+        foreach ($this->tokens() as $name => $token) {
+            $values[$name] = $token ? $token->value() : null;
+        }
+
+        return $values;
+    }
+
+    abstract public function readToken(string $name, Replacement $replacement): void;
+
+    protected function commandLineOption(Replacement $replacement): ?string
+    {
+        $option = $replacement->optionName();
+        return $option ? ($this->options[$option] ?? null) : null;
+    }
+
+    protected function inputString(Replacement $replacement, string $default): string
+    {
+        $prompt = $replacement->inputPrompt();
+        if (!$prompt || !$this->interactive) { return $default; }
+
+        $promptPostfix = $default ? ' [default: `' . $default . '`]:' : ':';
+        return $this->env->input()->value($prompt . $promptPostfix) ?: $default;
+    }
+
+    protected function metaDataValue(string $namespace): string
+    {
+        return $this->env->metaData()->value($namespace) ?? '';
+    }
+
+    private function tokens(): array
+    {
+        if (!$this->tokens) { $this->replacements->tokens($this); }
+        return $this->tokens;
+    }
+}
---
+++ src/Application/Token/Reworked/Reader/UpdateReader.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of Shudd3r/Package-Files package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Shudd3r\PackageFiles\Application\Token\Reworked\Reader;
+
+use Shudd3r\PackageFiles\Application\Token\Reworked\Reader;
+use Shudd3r\PackageFiles\Replacement;
+
+
+class UpdateReader extends Reader
+{
+    public function readToken(string $name, Replacement $replacement): void
+    {
+        $value = $this->inputString($replacement, $this->commandLineOption($replacement) ?? $this->metaDataValue($name));
+        $this->tokens[$name] = $replacement->token($name, $value);
+    }
+}
