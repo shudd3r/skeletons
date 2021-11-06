de03d2437e028fe97be91ef4fdfb29cf3446ad40
Shudd3r <q3.shudder@gmail.com>
2021-11-06 08:54:33 +0100

    Refactored indexing of template file types

--- src/Setup/AppSetup.php
+++ src/Setup/AppSetup.php
@@ -68,29 +68,31 @@ class AppSetup
     {
         $typeIndex = [];
         foreach ($env->skeleton()->fileList() as $originalFile) {
-            $originalName = $originalFile->name();
-            [$type, $filename] = $this->targetFilename($originalName);
-            $typeIndex[$type][$filename] = $originalName;
+            $sourceName = $originalFile->name();
+            $fileType   = $this->fileType($sourceName);
+            $targetName = $this->targetFilename($sourceName, $fileType);
+            $typeIndex[$fileType][$targetName] = $sourceName;
         }
 
         return new Templates\TemplateFiles($env->skeleton(), $typeIndex);
     }
 
-    private function targetFilename(string $filename): array
+    private function fileType(string $filename): string
     {
         $extFound = strrpos($filename, self::EXT_PREFIX);
-        if (!$extFound) { return ['orig', $filename]; }
+        if (!$extFound) { return 'orig'; }
 
         $directive = substr($filename, $extFound);
-        if (!in_array($directive, self::EXT_TYPES)) { return ['orig', $filename]; }
-        $filename = $this->unlockedPath(substr($filename, 0, -strlen($directive)));
+        if (!in_array($directive, self::EXT_TYPES)) { return 'orig'; }
 
-        $type = substr($directive, strlen(self::EXT_PREFIX));
-        return [$type, $filename];
+        return substr($directive, strlen(self::EXT_PREFIX));
     }
 
-    private function unlockedPath(string $filename): string
+    private function targetFilename(string $filename, string $type): string
     {
-        return str_replace(self::EXT_DIR, '', $filename);
+        if ($type === 'orig') { return $filename; }
+        $realPathFilename = str_replace(self::EXT_DIR . '/', '/', $filename);
+        if (strpos($realPathFilename, '//')) { $realPathFilename = $filename; }
+        return substr($realPathFilename, 0, -strlen(self::EXT_PREFIX . $type));
     }
 }
