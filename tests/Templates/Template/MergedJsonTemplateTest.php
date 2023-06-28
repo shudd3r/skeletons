34227cffab0c59651e5d27055ed90ed1d4306d23
shudd3r <q3.shudder@gmail.com>
2023-06-28 07:32:30 +0200

    Refactored tests for MergedJsonTemplate

--- tests/Templates/Template/MergedJsonTemplateTest.php
+++ tests/Templates/Template/MergedJsonTemplateTest.php
@@ -33,78 +33,78 @@ class MergedJsonTemplateTest extends TestCase
      */
     public function testWithoutContentsToMerge_ReturnsOriginalTemplateRender(string $contents)
     {
-        $this->assertSame($contents, $this->template($contents, '')->render(self::$token));
-        $this->assertSame('not json', $this->template('not json', $contents)->render(self::$token));
+        $this->assertSame($contents, $this->jsonTemplate($contents, '')->render(self::$token));
+        $this->assertSame('not json', $this->jsonTemplate('not json', $contents)->render(self::$token));
     }
 
     public function testDecoratedTemplate_IsRenderedWithProvidedToken()
     {
-        $template = json_encode(['foo' => '{replace.me}', 'bar' => 'value']);
-        $package  = json_encode(['baz' => 'merged']);
+        $template = ['foo' => '{replace.me}', 'bar' => 'value'];
+        $package  = ['baz' => 'merged'];
         $expected = ['foo' => 'replaced', 'bar' => 'value', 'baz' => 'merged'];
         $this->assertJsonData($expected, $this->template($template, $package));
     }
 
     public function testForFlatArrays_ReturnsMergedJsonMatchingTemplateStructure()
     {
-        $template = json_encode(['first' => 'template_first', 'bar' => 'template_bar']);
-        $package  = json_encode(['foo' => 'package_foo', 'bar' => 'package_bar']);
+        $template = ['first' => 'template_first', 'bar' => 'template_bar'];
+        $package  = ['foo' => 'package_foo', 'bar' => 'package_bar'];
         $expected = ['first' => 'template_first', 'bar' => 'template_bar', 'foo' => 'package_foo'];
         $this->assertJsonData($expected, $this->template($template, $package));
     }
 
     public function testNullValuesAfterMergeAreFiltered()
     {
-        $template = json_encode(['first' => null, 'bar' => null]);
-        $package  = json_encode(['foo' => 'package_foo', 'bar' => 'package_bar']);
+        $template = ['first' => null, 'bar' => null];
+        $package  = ['foo' => 'package_foo', 'bar' => 'package_bar'];
         $expected = ['bar' => 'package_bar', 'foo' => 'package_foo'];
         $this->assertJsonData($expected, $this->template($template, $package));
     }
 
     public function testForNestedArrays_ReturnsStructureWithNestedArraysMatchingTemplateStructure()
     {
-        $template = json_encode(['first' => 'tpl', 'bar' => ['nest1' => 'tpl', 'nest2' => null, 'nest3' => 'tpl']]);
-        $package  = json_encode(['first' => 'pkg', 'bar' => ['nest1' => 'pkg', 'nest2' => 'pkg', 'nest4' => 'pkg']]);
+        $template = ['first' => 'tpl', 'bar' => ['nest1' => 'tpl', 'nest2' => null, 'nest3' => 'tpl']];
+        $package  = ['first' => 'pkg', 'bar' => ['nest1' => 'pkg', 'nest2' => 'pkg', 'nest4' => 'pkg']];
         $expected = ['first' => 'tpl', 'bar' => ['nest1' => 'tpl', 'nest2' => 'pkg', 'nest3' => 'tpl', 'nest4' => 'pkg']];
         $this->assertJsonData($expected, $this->template($template, $package));
     }
 
     public function testForNestedLists_ReturnsListsWithCombinedValues()
     {
-        $template = json_encode(['list' => ['foo', 'bar', 'baz']]);
-        $package  = json_encode(['list' => ['package1', 'baz', 'foo', 'package2']]);
+        $template = ['list' => ['foo', 'bar', 'baz']];
+        $package  = ['list' => ['package1', 'baz', 'foo', 'package2']];
         $expected = ['list' => ['foo', 'bar', 'baz', 'package1', 'package2']];
         $this->assertJsonData($expected, $this->template($template, $package));
     }
 
     public function testFirstArrayInTemplateList_IsUsedAsStructureTemplateForAllItems()
     {
-        $template = json_encode(['list' => [['a' => 1, 'b' => 1]]]);
-        $package  = json_encode(['list' => [['b' => 2, 'a' => 2, 'c' => 2], ['c' => 3, 'a' => 3], ['b' => 1, 'a' => 1]]]);
+        $template = ['list' => [['a' => 1, 'b' => 1]]];
+        $package  = ['list' => [['b' => 2, 'a' => 2, 'c' => 2], ['c' => 3, 'a' => 3], ['b' => 1, 'a' => 1]]];
         $expected = ['list' => [['a' => 1, 'b' => 1], ['a' => 2, 'b' => 2, 'c' => 2], ['a' => 3, 'c' => 3]]];
         $this->assertJsonData($expected, $this->template($template, $package));
     }
 
     public function testFirstArrayInTemplateListWithNullValues_IsCombinedWithItemsWithoutNullValues()
     {
-        $template = json_encode([['a' => null, 'b' => 1, 'c' => null]]);
-        $package  = json_encode([['b' => 2, 'a' => 2, 'c' => 2], ['c' => 3, 'a' => 3], ['b' => 1]]);
+        $template = [['a' => null, 'b' => 1, 'c' => null]];
+        $package  = [['b' => 2, 'a' => 2, 'c' => 2], ['c' => 3, 'a' => 3], ['b' => 1]];
         $expected = [['b' => 1], ['a' => 2, 'b' => 2, 'c' => 2], ['a' => 3, 'c' => 3]];
         $this->assertJsonData($expected, $this->template($template, $package));
     }
 
     public function testFirstArrayInListWithNullValuesOnly_IsUsedOnlyAsTemplateAndNotMergedIntoList()
     {
-        $template = json_encode([['a' => null, 'b' => null]]);
-        $package  = json_encode([['b' => 1, 'a' => 1, 'c' => 1], ['c' => 2, 'a' => 2]]);
+        $template = [['a' => null, 'b' => null]];
+        $package  = [['b' => 1, 'a' => 1, 'c' => 1], ['c' => 2, 'a' => 2]];
         $expected = [['a' => 1, 'b' => 1, 'c' => 1], ['a' => 2, 'c' => 2]];
         $this->assertJsonData($expected, $this->template($template, $package));
     }
 
     public function testEmptyTemplateListAfterMerge_IsFiltered()
     {
-        $template = json_encode(['foo' => 'foo-value', 'list' => [['a' => null, 'b' => null]]]);
-        $package  = json_encode(['bar' => 'bar-value']);
+        $template = ['foo' => 'foo-value', 'list' => [['a' => null, 'b' => null]]];
+        $package  = ['bar' => 'bar-value'];
         $expected = ['foo' => 'foo-value', 'bar' => 'bar-value'];
         $this->assertJsonData($expected, $this->template($template, $package));
     }
@@ -112,16 +112,16 @@ class MergedJsonTemplateTest extends TestCase
     public function testTypeOfEmptyStructureAfterMerge_IsBasedOnTemplateType()
     {
         $template = json_encode(['first' => null, 'bar' => null]);
-        $this->assertSame('{}', trim($this->template($template, '[]')->render(self::$token)));
+        $this->assertSame('{}', trim($this->jsonTemplate($template, '[]')->render(self::$token)));
 
         $template = json_encode([['a' => null, 'b' => null]]);
-        $this->assertSame('[]', trim($this->template($template, '{}')->render(self::$token)));
+        $this->assertSame('[]', trim($this->jsonTemplate($template, '{}')->render(self::$token)));
     }
 
     public function testUpdatedKeysInSynchronizedStructures_AreMerged()
     {
-        $template = json_encode(['foo' => null, 'updated_key' => 'something']);
-        $package  = json_encode(['foo' => 'value', 'old_key' => 'something', 'bar' => 'value']);
+        $template = ['foo' => null, 'updated_key' => 'something'];
+        $package  = ['foo' => 'value', 'old_key' => 'something', 'bar' => 'value'];
 
         $initialMerge = ['foo' => 'value', 'updated_key' => 'something', 'old_key' => 'something', 'bar' => 'value'];
         $this->assertJsonData($initialMerge, $this->template($template, $package));
@@ -176,10 +176,15 @@ class MergedJsonTemplateTest extends TestCase
         $template = self::$files->contentsOf('template-composer.json');
         $package  = self::$files->contentsOf($packageStateFile);
 
-        return $this->template($template, $package, $synchronized);
+        return $this->jsonTemplate($template, $package, $synchronized);
     }
 
-    private function template(string $template, string $package, bool $synchronized = false): Template
+    private function template(array $template, array $package, bool $synchronized = false): Template
+    {
+        return $this->jsonTemplate(json_encode($template), json_encode($package), $synchronized);
+    }
+
+    private function jsonTemplate(string $template, string $package, bool $synchronized = false): Template
     {
         $template = new Template\BasicTemplate($template);
         return new Template\MergedJsonTemplate($template, $package, $synchronized);
