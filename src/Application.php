f89b287b7dc877c8f36d2b773a35192fa1d2bea1
shudd3r <q3.shudder@gmail.com>
2025-01-01 22:52:41 +0100

    Updated app version for 1.0.2 release

--- src/Application.php
+++ src/Application.php
@@ -22,7 +22,7 @@ use Exception;
 
 class Application
 {
-    private const VERSION = '1.0.1';
+    private const VERSION = '1.0.2';
 
     private EnvSetup $envSetup;
     private AppSetup $appSetup;
